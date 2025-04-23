15046456818845b5fb654320731b9b0a2bf9bb31
Анатолий <homenkotolya2@gmail.com>
2025-04-23 22:36:25 +0700

    Добавьте файлы проекта.

---
+++ Multithreading/Program.cs
@@ -0,0 +1,13 @@
+﻿
+class Program
+{
+    static void Main(string[] args)
+    {
+        ProgramBase printer = new ProgramBase();
+        Thread thread = new Thread(printer.PrintNumbers);
+        thread.Start();
+
+        Console.WriteLine("Основной поток завершён.");
+        Console.ReadLine(); 
+    }
+}
---
+++ Multithreading/ProgramBase.cs
@@ -0,0 +1,11 @@
+﻿public class ProgramBase
+{
+    public void PrintNumbers()
+    {
+        for (int i = 1; i <= 10; i++)
+        {
+            Console.WriteLine(i);
+            Thread.Sleep(500); 
+        }
+    }
+}
