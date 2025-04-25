ffd0b8675d65df83bdbe81fc20f7e9f9381dcfde
Анатолий <homenkotolya2@gmail.com>
2025-04-25 23:38:16 +0700

    Создал метод, принимающий строку и выводящий её по буквам "Привет" с задержкой. Запустил этот метод в потоке, передав параметр через ParameterizedThreadStart.

--- Multithreading/Program.cs
+++ Multithreading/Program.cs
@@ -1,18 +1,23 @@
-﻿class Program
+﻿using System.Threading;
+
+class Program
 {
     static void Main(string[] args)
     {
         Thread thread1 = new Thread(PrintNumbers);
         Thread thread2 = new Thread(PrintEven);
         Thread thread3 = new Thread(PrintOdd);
-       
+        Thread thread4 = new Thread(new ParameterizedThreadStart(PrintLetters));
+
         thread1.Start();
         thread2.Start();
         thread3.Start();
+        thread4.Start("Привет");
 
         thread1.Join();
         thread2.Join();
         thread3.Join();
+        thread4.Join();
 
         Console.WriteLine("Потоки запущены. Нажмите Enter для выхода.");
         Console.ReadLine();
@@ -42,6 +47,17 @@
         {
             Console.WriteLine($"Нечётное: {i}");
             Thread.Sleep(500);
+        }
+    }
+    static void PrintLetters(object text)
+    {
+        if (text is string str)
+        {
+            foreach (char c in str)
+            {
+                Console.WriteLine(c);
+                Thread.Sleep(150); 
+            }
         }
     }
 }
