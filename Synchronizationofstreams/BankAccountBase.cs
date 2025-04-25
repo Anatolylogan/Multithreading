5fce0989a529009a97e4ee4a9736cb9b4796af8c
Анатолий <homenkotolya2@gmail.com>
2025-04-25 23:49:51 +0700

    Cмодылировал ситуацию с банкоматом. У меня есть общий счет, и несколько потоков (пользователей) пытаются снять с него деньги. Использовал lock для предотвращения гонки данных и ухода в минус. BankAccount — класс, представляющий банковский счёт с методом Withdraw(); lock (lockObj) — гарантирует, что только один поток одновременно может снимать деньги Thread.Join() — ждём завершения всех операций перед выводом финального баланса

---
+++ Synchronizationofstreams/BankAccountBase.cs
@@ -0,0 +1,30 @@
+﻿public class BankAccount
+{
+    private object lockObj = new object();
+
+    public int Balance { get; private set; }
+
+    public BankAccount(int initialBalance)
+    {
+        Balance = initialBalance;
+    }
+
+    public void Withdraw(int amount)
+    {
+        lock (lockObj) 
+        {
+            Console.WriteLine($"Пользователь пытается снять {amount} . Текущий баланс: {Balance}");
+
+            if (Balance >= amount)
+            {
+                Thread.Sleep(100);
+                Balance -= amount;
+                Console.WriteLine($"Успешно снято: {amount}. Остаток: {Balance}");
+            }
+            else
+            {
+                Console.WriteLine($"Недостаточно средств для снятия {amount}. Остаток: {Balance}");
+            }
+        }
+    }
+}
---
+++ Synchronizationofstreams/Program.cs
@@ -0,0 +1,27 @@
+﻿class Program
+{
+    static void Main(string[] args)
+    {
+        BankAccount account = new BankAccount(100);
+
+        Thread user1 = new Thread(() => WithdrawMoney(account, 70));
+        Thread user2 = new Thread(() => WithdrawMoney(account, 50));
+        Thread user3 = new Thread(() => WithdrawMoney(account, 30));
+
+        user1.Start();
+        user2.Start();
+        user3.Start();
+
+        user1.Join();
+        user2.Join();
+        user3.Join();
+
+        Console.WriteLine($"Операции завершены. Ваш баланс: {account.Balance}");
+    }
+
+    static void WithdrawMoney(BankAccount account, int amount)
+    {
+        account.Withdraw(amount);
+    }
+}
+
