0dc42feb5e68c7ab17372ea7bc3ddc75b8e0b75b
Анатолий <homenkotolya2@gmail.com>
2025-04-27 22:19:56 +0700

    Выполнил задачу 7.  Все потоки пытаются записать в файл. Благодаря Mutex, в один момент времени только один поток может войти в критическую секцию (mutex.WaitOne() mutex.ReleaseMutex()). Остальные потоки ждут, пока мьютекс освободится.

---
+++ Mutex/Program.cs
@@ -0,0 +1,37 @@
+﻿class Program
+{
+    static Mutex mutex = new Mutex();
+
+    static void Main(string[] args)
+    {
+        for (int i = 0; i < 5; i++)
+        {
+            int threadId = i; 
+            Thread thread = new Thread(() => WriteToFile(threadId));
+            thread.Start();
+        }
+
+        Console.WriteLine("Нажмите Enter для выхода...");
+        Console.ReadLine();
+    }
+
+    static void WriteToFile(int threadId)
+    {
+        Console.WriteLine($"Поток {threadId} ожидает доступ к файлу...");
+
+        mutex.WaitOne(); 
+        try
+        {
+            Console.WriteLine($"Поток {threadId} начал запись в файл...");
+       
+            File.AppendAllText("output.txt", $"Поток {threadId} записал данные в {DateTime.Now}");
+
+            Thread.Sleep(1000); 
+            Console.WriteLine($"Поток {threadId} завершил запись.");
+        }
+        finally
+        {
+            mutex.ReleaseMutex(); 
+        }
+    }
+}
