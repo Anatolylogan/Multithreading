841f25bde0c6bd77122aaa237c70fbbd43485704
Анатолий <homenkotolya2@gmail.com>
2025-04-27 22:25:13 +0700

    Реализовал модель парковки: есть ограниченное число парковочных мест (например, 3). Потоки-автомобили пытаются припарковаться. Использовал Semaphore для ограничения доступа к парковке. Метод WaitOne() — поток останавливается и ждет, пока не будет доступно свободное место.Метод Release() — освобождает место на парковке, увеличивая количество доступных слотов в семафоре.Thread.Sleep — симулирует время стоянки на парковке

---
+++ Semaphore/Program.cs
@@ -0,0 +1,32 @@
+﻿class Program
+{
+    static Semaphore parkingSemaphore = new Semaphore(3, 3);
+
+    static void Main()
+    {
+        for (int i = 1; i <= 10; i++)
+        {
+            int carId = i;
+            Thread carThread = new Thread(() => TryToPark(carId));
+            carThread.Start();
+        }
+
+        Console.ReadLine();
+    }
+
+    static void TryToPark(int carId)
+    {
+        Console.WriteLine($"Машина {carId} подъехала к парковке и ждет свободное место.");
+
+        parkingSemaphore.WaitOne();
+
+        Console.WriteLine($"Машина {carId} припарковалась.");
+
+
+        Thread.Sleep(new Random().Next(2000, 5000));
+
+        Console.WriteLine($"Машина {carId} покидает парковку.");
+
+        parkingSemaphore.Release();
+    }
+}
