db41f736062fe0f5bb88bf54d4ccf11891c49c47
Анатолий <homenkotolya2@gmail.com>
2025-04-29 22:08:29 +0700

    Выполнил задачу . async Task<int> GetPageLengthAsync(string url) Асинхронно скачивает HTML и возвращает его длину.HttpClient.GetStringAsync Скачивает содержимое страницы.Task.WhenAll() Ожидает завершения всех задач и собирает их результаты.List<Task<int>> Хранит задачи, которые возвращают int.await Асинхронно ожидает завершения операций.

---
+++ AsyncAwait/Program.cs
@@ -0,0 +1,35 @@
+﻿class Program
+{
+    static async Task Main()
+    {
+        List<string> urls = new List<string>
+        {
+            "https://www.microsoft.com",
+            "https://www.google.com"
+        };
+
+        List<Task<int>> tasks = new List<Task<int>>();
+
+        foreach (var url in urls)
+        {
+            tasks.Add(GetPageLengthAsync(url));
+        }
+
+        int[] results = await Task.WhenAll(tasks);
+
+        for (int i = 0; i < urls.Count; i++)
+        {
+            Console.WriteLine($"Сайт: {urls[i]}, длина HTML: {results[i]} символов");
+        }
+
+        Console.WriteLine("Нажмите Enter для выхода.");
+        Console.ReadLine();
+    }
+
+    static async Task<int> GetPageLengthAsync(string url)
+    {
+        using HttpClient client = new HttpClient();
+        string html = await client.GetStringAsync(url);
+        return html.Length;
+    }
+}
